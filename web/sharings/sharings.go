7c523c21b569770701cc3810363a3f74997ac769
Gara64 <paul@cozycloud.cc>
2017-02-13 20:13:09 +0100

    Make recipients as sharing relationship

--- web/sharings/sharings.go
+++ web/sharings/sharings.go
@@ -3,31 +3,65 @@ package sharings
 import (
 	"net/http"
 
+	"github.com/cozy/cozy-stack/pkg/consts"
+	"github.com/cozy/cozy-stack/pkg/instance"
 	"github.com/cozy/cozy-stack/pkg/sharings"
 	"github.com/cozy/cozy-stack/web/jsonapi"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/labstack/echo"
 )
 
-// CreateSharing initializes a sharing by creating the associated document
+func extractRecipients(c echo.Context, instance *instance.Instance) error {
+
+	//TODO: extract the ResourceIdentifier from JSON and get the recipients doc
+
+	return nil
+}
+
+func checkStruct(c echo.Context, sharing *sharings.Sharing) error {
+
+	//check sharing type field
+	sharingType := sharing.SharingType
+	if sharingType != consts.OneShotSharing &&
+		sharingType != consts.MasterSlaveSharing &&
+		sharingType != consts.MasterMasterSharing {
+		err := sharings.ErrBadSharingType
+		return jsonapi.InvalidParameter("sharing_type", err)
+	}
+
+	//TODO: check other fiels?
+
+	return nil
+}
+
+// CreateSharing initializes a sharing by creating a document
 func CreateSharing(c echo.Context) error {
+
 	instance := middlewares.GetInstance(c)
 
+	//check recipients
+	if err := extractRecipients(c, instance); err != nil {
+		return err
+	}
+
+	//get the sharing
 	sharing := new(sharings.Sharing)
 	if err := c.Bind(sharing); err != nil {
 		return err
 	}
 
-	if err := sharings.CheckSharingCreation(instance, sharing); err != nil {
-		return wrapErrors(err)
+	//check sharing fields
+	if err := checkStruct(c, sharing); err != nil {
+		return err
 	}
 
+	// create document
 	doc, err := sharings.Create(instance, sharing)
 	if err != nil {
 		return err
 	}
 
-	return jsonapi.Data(c, http.StatusCreated, doc, nil)
+	return jsonapi.Data(c, http.StatusOK, doc, nil)
 }
 
 // Routes sets the routing for the sharing service
